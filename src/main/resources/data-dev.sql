f559b1ce3761cb0947613da35878a9a557dab8f4
devWhyqueue <dev.yannik.queisler@gmail.com>
2020-03-04 23:32:55 +0100

    Prepare for Heroku

---
+++ src/main/resources/data-dev.sql
@@ -0,0 +1,67 @@
+DELETE FROM Registration WHERE true;
+DELETE FROM Tournament_Discipline WHERE true;
+DELETE FROM Discipline WHERE true;
+DELETE FROM Tournament WHERE true;
+DELETE FROM Division WHERE true;
+DELETE FROM Batman_User WHERE true;
+DELETE FROM Player WHERE true;
+
+INSERT INTO Tournament (id, name, start, "end", close_of_entries) VALUES (1, 'Turnier 1', '2020-02-01', '2020-02-03', '2020-01-01');
+INSERT INTO Tournament (id, name, start, "end", close_of_entries) VALUES (2, 'Stauseepokal 2020', '2021-06-20', '2021-06-21', '2021-06-14');
+
+INSERT INTO Division (name) VALUES ('A');
+INSERT INTO Division (name) VALUES ('B');
+
+INSERT INTO Discipline (id, discipline_type, field_type, division_name) VALUES (1, 0, 0, 'A');
+INSERT INTO Discipline (id, discipline_type, field_type, division_name) VALUES (2, 1, 0, 'A');
+INSERT INTO Discipline (id, discipline_type, field_type, division_name) VALUES (3, 2, 2, 'A');
+INSERT INTO Discipline (id, discipline_type, field_type, division_name) VALUES (4, 0, 1, 'A');
+INSERT INTO Discipline (id, discipline_type, field_type, division_name) VALUES (5, 1, 1, 'A');
+INSERT INTO Discipline (id, discipline_type, field_type, division_name) VALUES (6, 0, 0, 'B');
+INSERT INTO Discipline (id, discipline_type, field_type, division_name) VALUES (7, 1, 0, 'B');
+INSERT INTO Discipline (id, discipline_type, field_type, division_name) VALUES (8, 2, 2, 'B');
+INSERT INTO Discipline (id, discipline_type, field_type, division_name) VALUES (9, 0, 1, 'B');
+INSERT INTO Discipline (id, discipline_type, field_type, division_name) VALUES (10, 1, 1, 'B');
+
+INSERT INTO Tournament_Discipline (id, capacity, registration_fee, tournament_id, discipline_id) VALUES (1, 8, 5, 2, 1);
+INSERT INTO Tournament_Discipline (id, capacity, registration_fee, tournament_id, discipline_id) VALUES (2, 8, 10.5, 2, 2);
+INSERT INTO Tournament_Discipline (id, capacity, registration_fee, tournament_id, discipline_id) VALUES (3, 8, 10.5, 2, 3);
+INSERT INTO Tournament_Discipline (id, capacity, registration_fee, tournament_id, discipline_id) VALUES (4, 8, 5, 2, 4);
+INSERT INTO Tournament_Discipline (id, capacity, registration_fee, tournament_id, discipline_id) VALUES (5, 8, 10.5, 2, 5);
+INSERT INTO Tournament_Discipline (id, capacity, registration_fee, tournament_id, discipline_id) VALUES (6, 8, 5, 2, 6);
+INSERT INTO Tournament_Discipline (id, capacity, registration_fee, tournament_id, discipline_id) VALUES (7, 8, 10.5, 2, 7);
+INSERT INTO Tournament_Discipline (id, capacity, registration_fee, tournament_id, discipline_id) VALUES (8, 8, 10.5, 2, 8);
+INSERT INTO Tournament_Discipline (id, capacity, registration_fee, tournament_id, discipline_id) VALUES (9, 8, 5, 2, 9);
+INSERT INTO Tournament_Discipline (id, capacity, registration_fee, tournament_id, discipline_id) VALUES (10, 8, 10.5, 2, 10);
+
+INSERT INTO Batman_User (id) VALUES (1);
+INSERT INTO Batman_User (id) VALUES (2);
+INSERT INTO Batman_User (id) VALUES (3);
+INSERT INTO Batman_User (id) VALUES (4);
+INSERT INTO Batman_User (id) VALUES (9);
+INSERT INTO Batman_User (id) VALUES (10);
+
+INSERT INTO Player (id, first_name, last_name, gender, club) VALUES (5, 'Jana', 'Meier', 1, 'SV Meiershoven');
+INSERT INTO Player (id, first_name, last_name, gender, club) VALUES (6, 'Janine', 'Meier', 1, 'SV Meiershoven');
+INSERT INTO Player (id, first_name, last_name, gender, club) VALUES (7, 'Jonas', 'Meier', 0, 'SV Meiershoven');
+INSERT INTO Player (id, first_name, last_name, gender, club) VALUES (8, 'Justus', 'Meier', 0, 'SV Meiershoven');
+INSERT INTO Player (id, first_name, last_name, gender, club) VALUES (11, 'Markus', 'Müller', 0, 'SV Müllersbach');
+INSERT INTO Player (id, first_name, last_name, gender, club) VALUES (12, 'Magdalena', 'Müller', 1, 'SV Müllersbach');
+
+INSERT INTO Registration (id, state, registration_date, user_id, partner_id, tournament_discipline_id) VALUES (1, 0, '2020-03-01', 1, null, 1);
+INSERT INTO Registration (id, state, registration_date, user_id, partner_id, tournament_discipline_id) VALUES (2, 1, '2020-03-01', 2, null, 1);
+INSERT INTO Registration (id, state, registration_date, user_id, partner_id, tournament_discipline_id) VALUES (3, 1, '2020-03-01', 3, null, 4);
+INSERT INTO Registration (id, state, registration_date, user_id, partner_id, tournament_discipline_id) VALUES (4, 1, '2020-03-01', 4, null, 4);
+INSERT INTO Registration (id, state, registration_date, user_id, partner_id, tournament_discipline_id) VALUES (5, 1, '2020-03-02', 2, 8, 2);
+INSERT INTO Registration (id, state, registration_date, user_id, partner_id, tournament_discipline_id) VALUES (6, 1, '2020-03-01', 3, 5, 5);
+INSERT INTO Registration (id, state, registration_date, user_id, partner_id, tournament_discipline_id) VALUES (7, 1, '2020-03-01', 4, 6, 5);
+INSERT INTO Registration (id, state, registration_date, user_id, partner_id, tournament_discipline_id) VALUES (8, 2, '2020-03-01', 1, 7, 2);
+INSERT INTO Registration (id, state, registration_date, user_id, partner_id, tournament_discipline_id) VALUES (9, 3, '2020-03-01', 1, 5, 3);
+INSERT INTO Registration (id, state, registration_date, user_id, partner_id, tournament_discipline_id) VALUES (10, 1, '2020-03-01', 2, 6, 3);
+INSERT INTO Registration (id, state, registration_date, user_id, partner_id, tournament_discipline_id) VALUES (11, 1, '2020-03-01', 9, null, 6);
+INSERT INTO Registration (id, state, registration_date, user_id, partner_id, tournament_discipline_id) VALUES (12, 1, '2020-03-01', 10, null, 9);
+INSERT INTO Registration (id, state, registration_date, user_id, partner_id, tournament_discipline_id) VALUES (13, 1, '2020-03-01', 9, 11, 7);
+INSERT INTO Registration (id, state, registration_date, user_id, partner_id, tournament_discipline_id) VALUES (14, 1, '2020-03-01', 10, 12, 10);
+INSERT INTO Registration (id, state, registration_date, user_id, partner_id, tournament_discipline_id) VALUES (15, 1, '2020-03-01', 3, 11, 3);
+
+
---
+++ src/main/resources/schema-prod.sql
@@ -0,0 +1,14 @@
+create sequence hibernate_sequence start 1 increment 1;
+create table batman_user (id int8 not null, primary key (id));
+create table discipline (id int8 not null, discipline_type int4, field_type int4, division_name varchar(255), primary key (id));
+create table division (name varchar(255) not null, primary key (name));
+create table player (id int8 not null, club varchar(255), first_name varchar(255), gender int4, last_name varchar(255), primary key (id));
+create table registration (id int8 not null, registration_date date, state int4, partner_id int8, tournament_discipline_id int8, user_id int8, primary key (id));
+create table tournament (id int8 not null, close_of_entries date, end date, name varchar(255), start date, primary key (id));
+create table tournament_discipline (id int8 not null, capacity int4, registration_fee float8, discipline_id int8, tournament_id int8, primary key (id));
+alter table if exists discipline add constraint FKtq6u76woehchd6sntyacwaf9q foreign key (division_name) references division;
+alter table if exists registration add constraint FKhmyyu2ljiwo8x10kjfab1pkru foreign key (partner_id) references player;
+alter table if exists registration add constraint FKif3c0waonn41d3n17ci6j7erg foreign key (tournament_discipline_id) references tournament_discipline;
+alter table if exists registration add constraint FKce4x4ppqh1v7iyicau6psp5j7 foreign key (user_id) references batman_user;
+alter table if exists tournament_discipline add constraint FKmd3du7y64cywy8aea1l9terot foreign key (discipline_id) references discipline;
+alter table if exists tournament_discipline add constraint FK4nqu9yp0852tly8yewyen1p2 foreign key (tournament_id) references tournament;
